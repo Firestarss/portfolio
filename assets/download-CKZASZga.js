4547a6efbdb91ec0ff57396a93170f35b1b6653f
Florian Schwarzinger <firestarss88@gmail.com>
2026-04-17 18:19:51 -0400

    Updates

--- assets/download-CKZASZga.js
+++ assets/download-CKZASZga.js
@@ -1,4 +1,4 @@
-import{c as o}from"./index-Df7JLG1D.js";/**
+import{c as o}from"./index-DkFYsx6P.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
